5e513cfbb90d40c4cb5f0ad811ac4b372f0dc987
bronybro <bronybro@tutanota.com>
2021-03-11 20:41:53 +0300

    add working inline buttons

--- handlers/users/keyboards.py
+++ handlers/users/keyboards.py
@@ -1,6 +1,9 @@
-from aiogram.dispatcher.filters import Command
-from aiogram.types import Message
+import logging
 
+from aiogram.dispatcher.filters import Command
+from aiogram.types import Message, CallbackQuery
+
+from keyboards.inline.callback_datas import book_callback
 from keyboards.inline.about_keys import contact_buttons
 from keyboards.inline.book_keys import pagination
 from loader import dp
@@ -8,12 +11,27 @@ from loader import dp
 
 @dp.message_handler(Command("about"))
 async def bot_about(message: Message):
-    await message.answer(text="Контакты создателя:",
+    await message.answer(text="about this bot",
                          reply_markup=contact_buttons)
 
 
 @dp.message_handler(Command("read"))
 async def bot_about(message: Message):
-    # text = # сюда переменную с названием статьи
-    await message.answer(text="Это пример простой читалки",
+    await message.answer(text="example",
                          reply_markup=pagination)
+
+
+@dp.callback_query_handler(book_callback.filter(page="prev"))
+async def prev_page(call: CallbackQuery):
+    await call.answer(cache_time=5)
+    callback_data = call.data
+    logging.info(f"call = {callback_data}")
+    await call.message.answer("-")
+
+
+@dp.callback_query_handler(book_callback.filter(page="next"))
+async def next_page(call: CallbackQuery):
+    await call.answer(cache_time=5)
+    callback_data = call.data
+    logging.info(f"call = {callback_data}")
+    await call.message.answer("+")
--- keyboards/inline/book_keys.py
+++ keyboards/inline/book_keys.py
@@ -1,8 +1,8 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from keyboards.inline.callback_datas import book_callback
 
-pagination = InlineKeyboardMarkup(row_width=3, )
+pagination = InlineKeyboardMarkup(row_width=3)
 previous = InlineKeyboardButton(text="-", callback_data=book_callback.new(page="prev"))
-page = InlineKeyboardButton(text="1", callback_data="page:page")
-next_page = InlineKeyboardButton(text="+", callback_data="page:next")
-pagination.row(previous, page, next_page)
+page = InlineKeyboardButton(text="1", callback_data="name:page")
+next_page = InlineKeyboardButton(text="+", callback_data="name:next")
+pagination.row(previous,page,next_page)
